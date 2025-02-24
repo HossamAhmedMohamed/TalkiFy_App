fe31c0dba4c74ba97876053c708bcb6974d4847e
HossamAhmadMohamed <ugs.141252@ci.suez.edu.eg>
2025-02-24 19:19:29 +0200

    ui_is_done

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:chat_material3/screens/auth/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        themeMode: ThemeMode.system,
+        darkTheme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.red, brightness: Brightness.dark),
+        ),
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.blue, brightness: Brightness.light),
+          useMaterial3: true,
+        ),
+        home: const LoginScreen());
+  }
+}
